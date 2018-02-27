d237df29690f2469e8880a9a015a4b21d654f354
digid0c <digidoc22@gmail.com>
2018-02-28 01:17:54 +0200

    QUIZ-3. Enable CORS support, implement properties reading from application.properties file

--- backend/src/main/java/ee/ttu/tarkvaratehnika/configuration/ApplicationProperties.java
+++ backend/src/main/java/ee/ttu/tarkvaratehnika/configuration/ApplicationProperties.java
@@ -3,9 +3,23 @@ package ee.ttu.tarkvaratehnika.configuration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Configuration
 @ConfigurationProperties(prefix = "quizzifly")
+@Getter
 public class ApplicationProperties {
 	
 	public static final String API_PREFIX = "/quizzifly/api";
+	
+	private final CorsProperties cors = new CorsProperties();
+	
+	@Getter
+	@Setter
+	public static class CorsProperties {
+		private String[] allowedOrigins;
+		private String[] allowedMethods;
+		private String[] allowedHeaders;
+	}
 }
