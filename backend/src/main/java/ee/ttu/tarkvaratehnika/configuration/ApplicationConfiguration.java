84ec44eeb86bfa5873f8a76326ecd40609b5a6a0
digid0c <digidoc22@gmail.com>
2018-03-04 21:59:03 +0200

    QUIZ-4. Provide Hibernate configuration, update pom.xml

--- backend/src/main/java/ee/ttu/tarkvaratehnika/configuration/ApplicationConfiguration.java
+++ backend/src/main/java/ee/ttu/tarkvaratehnika/configuration/ApplicationConfiguration.java
@@ -1,10 +1,23 @@
 package ee.ttu.tarkvaratehnika.configuration;
 
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(ApplicationProperties.class)
 public class ApplicationConfiguration {
+	
+	@Autowired
+	private EntityManagerFactory entityManagerFactory;
+	
+	@Bean
+	public SessionFactory sessionFactory() {
+		return entityManagerFactory.unwrap(SessionFactory.class);
+	}
 
 }
