40b285ab24b1fc0cc7e3965f4e9c5ef1a3e305cc
maschk <maschk@ttu.ee>
2018-03-26 22:04:57 +0300

    first this branch

---
+++ frontend/src/CreateQuiz2/CreateQuiz2.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+//import ReactDOM from 'react-dom';
+import './CreateQuiz2.css';
+var number = 1;
+
+
+export default class MakeQuiz extends Component {
+	//registration() {
+	//	ReactDOM.render(<Registration/>, document.getElementById('root'));
+	//}
+
+	render() {
+	    return (
+	      <div>
+	      <link rel="stylesheet" type="text/css" href="CreateQuiz2/CreateQuiz2.css" />
+	        <h1 className="title_quiz2">Create Quiz2</h1>
+	        <form className="search-form">
+	          <input type="question" name="question" placeholder="Question" className="answer" required />
+	          <input type="answer" name="answer1" placeholder="Answer 1" className="answer" required />
+              <input type="answer" name="answer2" placeholder="Answer 2" className="answer" required />
+              <input type="answer" name="answer3" placeholder="Answer 3" className="answer" required />
+              <input type="answer" name="answer4" placeholder="Answer 4" className="answer" required />
+              <input type="submit"  name="submit" value="Submit" id="submit-button" />
+            </form>
+	      </div>
+	    );
+  	}
+}
