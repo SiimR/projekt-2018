e0eaf93a9ebc4c448dfb4bfd4f435098f36c273d
digid0c <digidoc22@gmail.com>
2018-04-11 03:55:34 +0300

    QUIZ-5. Implement new user data saving to database

--- backend/src/main/java/ee/ttu/tarkvaratehnika/service/UserService.java
+++ backend/src/main/java/ee/ttu/tarkvaratehnika/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ee.ttu.tarkvaratehnika.dao.UserRepository;
 import ee.ttu.tarkvaratehnika.entity.UserEntity;
+import ee.ttu.tarkvaratehnika.model.UserJsonModel;
 import ee.ttu.tarkvaratehnika.model.UserModel;
 
 @Service
@@ -28,7 +29,21 @@ public class UserService {
 				.build();
 	};
 	
+	public static final Function<UserJsonModel, UserEntity> USER_JSON_TO_ENTITY = json -> {
+		
+		if (json == null) {
+			return null;
+		}
+		
+		return UserEntity.builder()
+				.name(json.getName())
+				.email(json.getEmail())
+				.passwordHash(json.getPasswordHash())
+				.build();
+	};
+	
 	private UserRepository userRepository;
+	private JsonValidationService jsonValidationService;
 	
 	public UserModel findLogin(String name, String passwordHash) {
 		UserEntity userEntity = userRepository.findLogin(name, passwordHash);
@@ -40,9 +55,44 @@ public class UserService {
 		return USER_ENTITY_TO_MODEL.apply(userEntity);
 	}
 	
+	public Integer register(UserJsonModel newUserDetails) {
+		jsonValidationService.validateUserDetails(newUserDetails.getJsonContent());
+		
+		UserEntity userEntity = USER_JSON_TO_ENTITY.apply(newUserDetails);
+		validateUserUniqueConstraints(userEntity);
+		
+		return userRepository.save(userEntity);
+	}
+	
+	private void validateUserUniqueConstraints(UserEntity user) {
+		validateUniqueNameConstraint(user.getName());
+		validateUniqueEmailConstraint(user.getEmail());
+	}
+	
+	private void validateUniqueNameConstraint(String name) {
+		UserEntity user = userRepository.findByName(name);
+		
+		if (user != null) {
+			throw new RuntimeException("The provided user name '" + name + "' is in use already");
+		}
+	}
+	
+	private void validateUniqueEmailConstraint(String email) {
+		UserEntity user = userRepository.findByEmail(email);
+		
+		if (user != null) {
+			throw new RuntimeException("The provided user email '" + email + "' is in use already");
+		}
+	}
+	
 	@Autowired
 	public void setUserRepository(UserRepository userRepository) {
 		this.userRepository = userRepository;
+	}
+	
+	@Autowired
+	public void setJsonValidationService(JsonValidationService jsonValidationService) {
+		this.jsonValidationService = jsonValidationService;
 	}
 
 }
