bdd91f7fea1523887a0a0be90d6689930c25547e
maschk <maschk@ttu.ee>
2018-05-15 00:15:13 +0300

    timer and progress bar

--- frontend/src/TakeQuiz/TakeQuiz.js
+++ frontend/src/TakeQuiz/TakeQuiz.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import QuizPage from '../Quiz/QuizPage.js';
+import QuizPage from '../Quiz/QuizPage1.js';
 import axios from 'axios';
 
 
